2fa41c26db32a72944588082e892921ea92183b5
Sofiane Ros <sofianeros.professionnel@gmail.com>
2026-03-22 16:59:44 +0100

    add data fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,47 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // ... (le reste du code avec le User et le Product)
+        // 1. Création d'un faux compte Client
+        $user = new User();
+        $user->setEmail('client@test.com');
+        $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user, 'azerty'));
+        $user->setPseudo('GamerDu13');
+        $user->setAdresse('123 rue de la Victoire, 13000 Marseille');
+        $user->setCreatedAt(new \DateTime());
+        $user->setIsActive(true);
+
+        $manager->persist($user);
+
+        // 2. Création de 10 faux Produits
+        for ($i = 1; $i <= 10; $i++) {
+            $product = new Product();
+            $product->setNom('PC Gamer Edition Mega ' . $i);
+            $product->setPrix(999.99 + ($i * 50));
+            $product->setDescription('Machine de guerre numéro ' . $i);
+            $product->setStockQuantite(15);
+            $product->setIsActive(true);
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
